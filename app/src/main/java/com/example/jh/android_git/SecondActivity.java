6d8690a9a98f0808db53b08e2b096632a476c0ef
Administrator <1004260403@qq.com>
2017-04-19 12:02:17 +0800

    创建jin2.0分支上传。

--- app/src/main/java/com/example/jh/android_git/SecondActivity.java
+++ app/src/main/java/com/example/jh/android_git/SecondActivity.java
@@ -14,5 +14,9 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 创建jin2.0版本，并上传
+        setContentView(R.layout.activity_second);
+        // 创建2.0 下创建jin2.00版本，并没有jin2.00分支这样操作不行。
+        //目前git是2.0开始创建jin2.0 分支
     }
 }
