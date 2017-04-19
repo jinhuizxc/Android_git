2213dc58eb06b871c3181a6c9d98b5da9fe32904
Administrator <1004260403@qq.com>
2017-04-19 11:49:31 +0800

    创建hui2.0分支上传。

---
+++ app/src/main/java/com/example/jh/android_git/ThirdActivity.java
@@ -0,0 +1,20 @@
+package com.example.jh.android_git;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jinhui  on 2017/4/19
+ * 邮箱: 1004260403@qq.com
+ */
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+        // 创建hui2.0 并上传代码
+    }
+}
