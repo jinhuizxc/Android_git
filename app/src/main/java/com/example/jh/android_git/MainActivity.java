56f6dc033fd82d7013ad72517679b19b109a5a23
Administrator <1004260403@qq.com>
2017-04-19 10:32:35 +0800

    上传master第一个版本

---
+++ app/src/main/java/com/example/jh/android_git/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jh.android_git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 1.首先上传当前版本到git
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
