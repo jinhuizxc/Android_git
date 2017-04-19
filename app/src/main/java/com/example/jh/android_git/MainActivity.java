6f9cc1250f27bca74c360eb8c1bba9a1af7bcb13
Administrator <1004260403@qq.com>
2017-04-19 11:38:05 +0800

    创建jin2.0并上传。

--- app/src/main/java/com/example/jh/android_git/MainActivity.java
+++ app/src/main/java/com/example/jh/android_git/MainActivity.java
@@ -4,7 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 /**
- * 1.首先上传当前版本到git
+ *
+ * 1.首先上传当前master版本到git
+ * 2. 创建jin2.0 上传
+ * 3.创建hui2.0 上传
+ * 4.合并代码，上传
+ * 5.修改jin2.0 代码，merge合并
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 创建jin2.0分支，开始上传
     }
 }
---
+++ app/src/main/java/com/example/jh/android_git/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.jh.android_git;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jinhui  on 2017/4/19
+ * 邮箱: 1004260403@qq.com
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
