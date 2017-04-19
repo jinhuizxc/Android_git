37f261b772562d5d269a0f46eb048fc2a568a63f
Administrator <1004260403@qq.com>
2017-04-19 12:25:04 +0800

    更新jin2.0 实现界面跳转。

--- app/src/main/java/com/example/jh/android_git/MainActivity.java
+++ app/src/main/java/com/example/jh/android_git/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jh.android_git;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  *
@@ -12,12 +15,19 @@ import android.os.Bundle;
  * 5.合并代码，上传
  * 6.修改jin2.0 代码，merge合并
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 创建2.0分支，开始上传
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, SecondActivity.class));
     }
 }
