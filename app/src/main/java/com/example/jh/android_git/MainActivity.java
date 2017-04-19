cb0d474b3969786b25cb1dded72504012944afeb
Administrator <1004260403@qq.com>
2017-04-19 11:41:01 +0800

    创建2.0分支，开始上传

--- app/src/main/java/com/example/jh/android_git/MainActivity.java
+++ app/src/main/java/com/example/jh/android_git/MainActivity.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 /**
  *
  * 1.首先上传当前master版本到git
- * 2. 创建jin2.0 上传
- * 3.创建hui2.0 上传
- * 4.合并代码，上传
- * 5.修改jin2.0 代码，merge合并
+ * 2. 创建2.0版本
+ * 3. 创建jin2.0 上传
+ * 4.创建hui2.0 上传
+ * 5.合并代码，上传
+ * 6.修改jin2.0 代码，merge合并
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 创建jin2.0分支，开始上传
+        // 创建2.0分支，开始上传
     }
 }
