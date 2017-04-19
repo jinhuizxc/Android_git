ca1b3b1afeb1f8153ef8e530342707ec8c2b8cb6
Administrator <1004260403@qq.com>
2017-04-19 15:02:50 +0800

    将jin2.0代码合并2.0代码

--- app/src/main/java/com/example/jh/android_git/MainActivity.java
+++ app/src/main/java/com/example/jh/android_git/MainActivity.java
@@ -13,7 +13,8 @@ import android.widget.Button;
  * 3. 创建jin2.0 上传
  * 4.创建hui2.0 上传
  * 5.合并代码，上传
- * 6.修改jin2.0 代码，merge合并
+ * 6.首先要选中2.0 然后在merge 会将jin2.0代码合并到2.0上面
+ * 7. 接着将2.0代码上传，然后在hui2.0这里pull下来
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
